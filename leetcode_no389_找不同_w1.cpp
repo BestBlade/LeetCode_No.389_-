06f773766bfd4ecb7274310dc2ba54025730a43e
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-12-18 09:36:42 +0800

    Add files via upload

---
+++ leetcode_no389_找不同_w1.cpp
@@ -0,0 +1,72 @@
+﻿/* ------------------------------------------------------------------|
+给定两个字符串 s 和 t，它们只包含小写字母。
+
+字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+
+请找出在 t 中被添加的字母。
+
+ 
+
+示例 1：
+
+输入：s = "abcd", t = "abcde"
+输出："e"
+解释：'e' 是那个被添加的字母。
+示例 2：
+
+输入：s = "", t = "y"
+输出："y"
+示例 3：
+
+输入：s = "a", t = "aa"
+输出："a"
+示例 4：
+
+输入：s = "ae", t = "aea"
+输出："a"
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-the-difference
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：6.9 MB, 在所有 C++ 提交中击败了30.22%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+char findTheDifference(string s, string t) {
+    int res = 0;
+    for (char c : t) {
+        res += c;
+    }
+    for (char c : s) {
+        res -= c;
+    }
+    return res;
+}
